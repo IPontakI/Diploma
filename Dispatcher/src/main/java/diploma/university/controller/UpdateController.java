84bd12e2d3b2133a66ef8a48b1f7e70cc6838684
IPontakI <valik_bilokin@ukr.net>
2025-05-04 18:56:53 +0300

    commit 04.05.25 18:56

---
+++ Dispatcher/src/main/java/diploma/university/controller/UpdateController.java
@@ -0,0 +1,115 @@
+package diploma.university.controller;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import java.util.List;
+
+import diploma.university.service.UpdateProducer;
+import diploma.university.utils.MessageUtils;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static diploma.university.model.RabbitQueue.*;
+
+@Component
+@Log4j
+public class UpdateController {
+    private TgBot telegramBot;
+    private final MessageUtils messageUtils;
+    private final UpdateProducer updateProducer;
+
+    public UpdateController(MessageUtils messageUtils, UpdateProducer updateProducer){
+        this.messageUtils = messageUtils;
+        this.updateProducer = updateProducer;
+    }
+                                                      
+    public void registerBot(TgBot telegramBot){
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update){
+        if (update == null){
+            log.error("Received update is null");
+            return;
+        }
+
+        if (update.getMessage() != null){
+            distributeMessageByType(update);
+        }else {
+            log.error("Unsupported message type received:" + update);
+        }
+    }
+
+    private void distributeMessageByType(Update update) {
+        var message = update.getMessage();
+        if (message.getText() != null){
+            processTextMessage(update);
+        } else if (message.getDocument() != null){
+            processDocMessage(update);
+        }else if (message.getPhoto() != null){
+            processPhotoMessage(update);
+        }else{
+            setUnsupportedMessage(update);
+        }
+    }
+
+    private void setView(SendMessage sendMessage){
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+
+    private void setFileIsReceivedView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Файл отримано! Обробляємо...");
+        setView(sendMessage);
+    }
+
+    private void setUnsupportedMessage(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Непідтримуваний тип повідомлення!");
+        setView(sendMessage);
+    }
+
+    private void processPhotoMessage(Update update) {
+        updateProducer.produce(PHOTO_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processDocMessage(Update update) {
+        updateProducer.produce(DOC_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processTextMessage(Update update) {
+        updateProducer.produce(TEXT_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+
+//        String text = update.getMessage().getText();
+//        Long chatId = update.getMessage().getChatId();
+//
+//        if ("/start".equalsIgnoreCase(text)) {
+//            SendMessage startMessage = StartMenu(chatId);
+//            setView(startMessage);
+//            return;
+//        }
+    }
+
+//    private SendMessage StartMenu(Long chatId) {
+//        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+//        keyboardMarkup.setResizeKeyboard(true);
+//        keyboardMarkup.setOneTimeKeyboard(false); // щоб меню залишалося
+//
+//        KeyboardRow row = new KeyboardRow();
+//        row.add("Зареєструватися");
+//        row.add("Увійти");
+//
+//        keyboardMarkup.setKeyboard(List.of(row));
+//
+//        SendMessage message = new SendMessage();
+//        message.setChatId(chatId);
+//        message.setText("Створіть або увійдіть у свій обліковий запис:");
+//        message.setReplyMarkup(keyboardMarkup);
+//        return message;
+//    }
+}
